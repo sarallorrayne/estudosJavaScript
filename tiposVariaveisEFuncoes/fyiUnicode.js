67cd0d85c19d1fe2ee683b1547452b26f4f37bb1
Sara Souza <sarallorrayne@hotmail.com>
2024-11-13 12:31:35 -0300

    estudos sobre tipos de Variáveis e Unicode

--- tiposVariaveisEFuncoes/fyiUnicode.js
+++ tiposVariaveisEFuncoes/fyiUnicode.js
@@ -9,11 +9,3 @@ console.log(cifrao);
 console.log(aMaiusculo);
 console.log(check);
 console.log(hiragana); // Hiragana é um silabário japonês
-
-let valor1 = 0
-let abacate
-let valor2 = "10"
-
-console.log("valor1 é do tipo:", typeof valor1)
-console.log("abacate é do tipo:", typeof abacate)
-console.log("valor2 é do tipo:", typeof valor2)
